dc4d125dac77aa41075c88eee97722c530aaac17
Charles Grimont <charlesg@convertigo.com>
2017-09-04 17:08:45 +0200

    resolve an issue concerning the destroy on a null loading object when we make multiple call in the same time

--- src/caf/convertigo.page.ts
+++ src/caf/convertigo.page.ts
@@ -7,23 +7,26 @@ import { C8o } from "c8osdkangular";
 
 
 export class C8oPage {
-    public router;
-    public navParams;
-    private loader;
-    private didLoad;
-    private imgCache : Object = new Object();
-    private prefixId : string;
-    public form = {};
-    public c8o : C8o;
-    public menuId : string;
+  public router;
+  public navParams;
+  private loader;
+  private didLoad;
+  private imgCache : Object = new Object();
+  private prefixId : string;
+  public form = {};
+  public c8o : C8o;
+  public menuId : string;
+  private shown: boolean = false;
+  private finish: boolean = false;
+  private count: number = 0;
 
-    constructor(public routerProvider : C8oRouter, private navParam: NavParams, public loadingCtrl: LoadingController, private sanitizer : DomSanitizer, private ref: ChangeDetectorRef, private injector: Injector, private menuCtrl: MenuController){
-	      this.router = routerProvider;
-        this.c8o = this.router.c8o;
-        this.navParams = (navParam.get("navParams") != undefined && navParam.get("navParams") != null) ? navParam.get("navParams") : "";
-        this.router.storeResponseForView(this.constructor.name, navParam.get("requestable"), navParam.get("data"), this.navParams, navParam.get("didEnter") ,navParam.get("didLeave"));
-        this.prefixId = "_C8o" + new Date().getTime().toString();
-    }
+  constructor(public routerProvider : C8oRouter, private navParam: NavParams, public loadingCtrl: LoadingController, private sanitizer : DomSanitizer, private ref: ChangeDetectorRef, private injector: Injector, private menuCtrl: MenuController){
+    this.router = routerProvider;
+    this.c8o = this.router.c8o;
+    this.navParams = (navParam.get("navParams") != undefined && navParam.get("navParams") != null) ? navParam.get("navParams") : "";
+    this.router.storeResponseForView(this.constructor.name, navParam.get("requestable"), navParam.get("data"), this.navParams, navParam.get("didEnter") ,navParam.get("didLeave"));
+    this.prefixId = "_C8o" + new Date().getTime().toString();
+  }
 
   /**
    * Retrieves an instance from the injector based on the provided token.
@@ -37,73 +40,82 @@ export class C8oPage {
    *
    * @return An instance of the given token, or an error if not found
    */
-    public getInstance<T>(token: Type<T>|InjectionToken<T>, notFoundValue?: T): T{
-      return this.injector.get(token, notFoundValue);
-    }
+  public getInstance<T>(token: Type<T>|InjectionToken<T>, notFoundValue?: T): T{
+    return this.injector.get(token, notFoundValue);
+  }
 
-    /**
-     * Gets the data from previous called requestable list. can be used in an Angular 2 directive such as
-     *
-     *   *ngFor="let category of listen(['fs://.view']).rows" or
-     *   *ngFor="let Page2 of listen(['fs://.view', 'fs://.view#search']).rows"
-     *
-     * The data for the first requestable to match is returned
-     *
-     * @return the data for one of the requestables in the list.
-     */
-    public listen(requestables : string[]) : any {
-    	return this.router.getResponseForView(this.constructor.name, requestables);
-    }
+  /**
+   * Gets the data from previous called requestable list. can be used in an Angular 2 directive such as
+   *
+   *   *ngFor="let category of listen(['fs://.view']).rows" or
+   *   *ngFor="let Page2 of listen(['fs://.view', 'fs://.view#search']).rows"
+   *
+   * The data for the first requestable to match is returned
+   *
+   * @return the data for one of the requestables in the list.
+   */
+  public listen(requestables : string[]) : any {
+    return this.router.getResponseForView(this.constructor.name, requestables);
+  }
 
-    /**
-     * Gets the data from previous called requestable list. can be used in an Angular 2 directive such as
-     *
-     *   *ngFor="let category of listen(['fs://.view']).rows" or
-     *   *ngFor="let Page2 of listen(['fs://.view', 'fs://.view#search']).rows"
-     *
-     * The data for the first requestable to match is returned
-     *
-     * @return the data for one of the requestables in the list.
-     */
-    public listenNavParams(requestable : string) : any {
-    	return(this.router.getParamForView(this.constructor.name, requestable));
+  /**
+   * Gets the data from previous called requestable list. can be used in an Angular 2 directive such as
+   *
+   *   *ngFor="let category of listen(['fs://.view']).rows" or
+   *   *ngFor="let Page2 of listen(['fs://.view', 'fs://.view#search']).rows"
+   *
+   * The data for the first requestable to match is returned
+   *
+   * @return the data for one of the requestables in the list.
+   */
+  public listenNavParams(requestable : string) : any {
+    return(this.router.getParamForView(this.constructor.name, requestable));
+  }
+  callForm(requestable:string, id: string){
+    this.call(requestable, this.form[id]);
+  }
+  /**
+   * Calls a Convertigo requestable with parameters as Object
+   *
+   * @param	requestable the requestable to call (examples : "Myproject.MySequence" or "fs://MyLocalDataBase.get")
+   * @param	data , the data to send to the requestable (for example {"var1" : "value1, ..., "var2" : "value2})
+   *
+   */
+  public call(requestable, data: any = null, navParams : any = null, timeout : number = 500) {
+    if(this.form != {} && data == null){
+      data = this.form;
     }
-     callForm(requestable:string, id: string){
-      this.call(requestable, this.form[id]);
-     }
-    /**
-     * Calls a Convertigo requestable with parameters as Object
-     *
-     * @param	requestable the requestable to call (examples : "Myproject.MySequence" or "fs://MyLocalDataBase.get")
-     * @param	data , the data to send to the requestable (for example {"var1" : "value1, ..., "var2" : "value2})
-     *
-     */
-    public call(requestable, data: any = null, navParams : any = null, timeout : number = 500) {
-        if(this.form != {} && data == null){
-            data = this.form;
+    setTimeout(()=> {
+      if(this.finish == false){
+        if(this.shown != true){
+          this.loader = this.loadingCtrl.create({
+          });
+          this.loader.present();
+          this.shown = true;
         }
-        let shown = false;
-        let finish = false;
-        setTimeout(()=> {
-            if(finish == false){
-                this.loader = this.loadingCtrl.create({
-                });
-                this.loader.present();
-                shown = true;
-            }
-        }, timeout);
-        this.router.c8oCall(requestable, data, navParams, this).then(()=>{
-            finish = true;
-            if(shown == true) {
-                this.loader.dismiss();
-                shown = false;
-            }
-        }).catch(()=>{
-            finish = true;
-            if(shown == true) this.loader.dismiss();
-        });
+        this.count ++;
+      }
+    }, timeout);
+    this.router.c8oCall(requestable, data, navParams, this).then(()=>{
+      this.finish = true;
+      if(this.shown == true) {
+        this.count --;
+        if(this.count == 0) {
+          this.shown = false;
+          this.loader.dismiss();
+        }
+      }
+    }).catch(()=>{
+      this.finish = true;
+      if(this.shown == true){
+        this.count --;
+        if(this.count == 0){
+          this.loader.dismiss();
+        }
+      }
+    });
 
-    }
+  }
   public tick(){
     this.ref.markForCheck();
   }
@@ -113,116 +125,113 @@ export class C8oPage {
     this.ref.detach();
   }
 
-    public ionViewDidLoad(){
+  public ionViewDidLoad(){
 
-        if(!(this.navParam.get("didLoad") == null || this.navParam.get("didLoad") == undefined || this.navParam.get("didLoad") == '')){
-           this.navParam.get("didLoad")(this, this.router.c8o);
-        }
+    if(!(this.navParam.get("didLoad") == null || this.navParam.get("didLoad") == undefined || this.navParam.get("didLoad") == '')){
+      this.navParam.get("didLoad")(this, this.router.c8o);
     }
+  }
 
-    public ionViewWillEnter(){
-        if(!(this.navParam.get("willEnter") == null || this.navParam.get("willEnter") == undefined || this.navParam.get("willEnter") == '')){
-           this.navParam.get("willEnter")(this, this.router.c8o);
-        }
+  public ionViewWillEnter(){
+    if(!(this.navParam.get("willEnter") == null || this.navParam.get("willEnter") == undefined || this.navParam.get("willEnter") == '')){
+      this.navParam.get("willEnter")(this, this.router.c8o);
     }
+  }
 
-    public ionViewDidEnter(){
-        if(!(this.menuId == null || this.menuId == undefined || this.menuId == '')) {
-          this.menuCtrl.enable(true, this.menuId);
-        }
-        this.didLoad = true;
-        if(!(this.navParam.get("didEnter") == null || this.navParam.get("didEnter") == undefined || this.navParam.get("didEnter") == '')){
-           this.navParam.get("didEnter")(this, this.router.c8o);
-        }
+  public ionViewDidEnter(){
+    if(!(this.menuId == null || this.menuId == undefined || this.menuId == '')) {
+      this.menuCtrl.enable(true, this.menuId);
     }
-
-    public ionViewWillLeave(){
-        if(!(this.navParam.get("willLeave") == null || this.navParam.get("willLeave") == undefined || this.navParam.get("willLeave") == '')){
-           this.navParam.get("willLeave")(this, this.router.c8o);
-        }
+    this.didLoad = true;
+    if(!(this.navParam.get("didEnter") == null || this.navParam.get("didEnter") == undefined || this.navParam.get("didEnter") == '')){
+      this.navParam.get("didEnter")(this, this.router.c8o);
     }
+  }
 
-    public ionViewDidLeave(){
-        if(!(this.menuId == null || this.menuId == undefined || this.menuId == '')) {
-          this.menuCtrl.enable(false, this.menuId);
-        }
-        if(!(this.navParam.get("didLeave") == null || this.navParam.get("didLeave") == undefined || this.navParam.get("didLeave") == '')){
-           this.navParam.get("didLeave")(this, this.router.c8o);
-        }
+  public ionViewWillLeave(){
+    if(!(this.navParam.get("willLeave") == null || this.navParam.get("willLeave") == undefined || this.navParam.get("willLeave") == '')){
+      this.navParam.get("willLeave")(this, this.router.c8o);
     }
+  }
 
-    public ionViewWillUnload(){
-        if(!(this.navParam.get("willUnLoad") == null || this.navParam.get("willUnLoad") == undefined || this.navParam.get("willUnLoad") == '')){
-           this.navParam.get("willUnLoad")(this, this.router.c8o);
-        }
+  public ionViewDidLeave(){
+    if(!(this.menuId == null || this.menuId == undefined || this.menuId == '')) {
+      this.menuCtrl.enable(false, this.menuId);
     }
+    if(!(this.navParam.get("didLeave") == null || this.navParam.get("didLeave") == undefined || this.navParam.get("didLeave") == '')){
+      this.navParam.get("didLeave")(this, this.router.c8o);
+    }
+  }
 
-    public virtualListen(arg: any){
-      if(arg == undefined){
-        return [];
+  public ionViewWillUnload(){
+    if(!(this.navParam.get("willUnLoad") == null || this.navParam.get("willUnLoad") == undefined || this.navParam.get("willUnLoad") == '')){
+      this.navParam.get("willUnLoad")(this, this.router.c8o);
+    }
+  }
+
+  public virtualListen(arg: any){
+    if(arg == undefined){
+      return [];
+    }
+    else{
+      return arg;
+    }
+  }
+
+  /**
+   * Get attachment data url a requestable response
+   *
+   * @param	requestables, target requestable list to listen to and build attachment urls (examples : "Myproject.MySequence" or "fs://MyLocalDataBase.get")
+   * @param	attachmentName , the name of the attachment to get
+   *
+   */
+  public getAttachmentUrl(id: string, attachmentName: string, placeholderURL : string): Object{
+
+    if(id != null){
+      if(this.imgCache[id+"/"+attachmentName] == undefined){
+        this.imgCache[id+"/"+attachmentName] = placeholderURL
+        this.router.c8o.get_attachment(id, attachmentName).then((response)=>{
+          this.imgCache[id+"/"+attachmentName] = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(response))
+        })
       }
-      else{
-        return arg;
+    }
+    return this.imgCache[id+"/"+attachmentName]
+  }
+
+  public getNextLetter(char: String): String {
+    let code: number = char.charCodeAt(0);
+    code ++;
+    return String.fromCharCode(code);
+  }
+
+  public wordPlusOne(word: string): any {
+    if (word != undefined) {
+      let word1 = word.slice(0, -1)
+      let word2 = this.getNextLetter(word)
+      return word1 + word2;
+    }
+    else {
+      return {};
+    }
+  }
+  public merge(firstObj: Object, secondObj): Object{
+    return Object.assign(firstObj, secondObj);
+  }
+
+  public getPageByTitle(pageTitle: string) {
+    for (let p of this.router.pagesArray){
+      if (p["title"] == pageTitle) {
+        return p.component;
       }
     }
+  }
 
-    /**
-     * Get attachment data url a requestable response
-     *
-     * @param	requestables, target requestable list to listen to and build attachment urls (examples : "Myproject.MySequence" or "fs://MyLocalDataBase.get")
-     * @param	attachmentName , the name of the attachment to get
-     *
-     */
-    public getAttachmentUrl(id: string, attachmentName: string, placeholderURL : string): Object{
-
-        if(id != null){
-            if(this.imgCache[id+"/"+attachmentName] == undefined){
-                this.imgCache[id+"/"+attachmentName] = placeholderURL
-                this.router.c8o.get_attachment(id, attachmentName).then((response)=>{
-                    this.imgCache[id+"/"+attachmentName] = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(response))
-                })
-            }
-        }
-        return this.imgCache[id+"/"+attachmentName]
+  public getPageByName(pageName: string) {
+    for (let p of this.router.pagesArray){
+      if (p["component"].name == pageName) {
+        return p.component;
+      }
     }
-
-    public getNextLetter(char: String): String {
-        let code: number = char.charCodeAt(0);
-        code ++;
-        return String.fromCharCode(code);
-    }
-
-    public wordPlusOne(word: string): any {
-        if (word != undefined) {
-            let word1 = word.slice(0, -1)
-            let word2 = this.getNextLetter(word)
-            return word1 + word2;
-        }
-        else {
-            return {};
-        }
-    }
-    public merge(firstObj: Object, secondObj): Object{
-      return Object.assign(firstObj, secondObj);
-    }
-
-    public getPageByTitle(pageTitle: string) {
-        for (let p of this.router.pagesArray){
-            if (p["title"] == pageTitle) {
-                return p.component;
-            }
-        }
-    }
-
-    public getPageByName(pageName: string) {
-        for (let p of this.router.pagesArray){
-            if (p["component"].name == pageName) {
-                return p.component;
-            }
-        }
-    }
+  }
 
 }
-
-
-
