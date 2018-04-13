6ee42c6ac7e82fa340382e64a9368dcdb46da95d
Grimont <grimont.c@gmail.com>
2018-04-13 14:34:51 +0200

    add export members

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./caf/convertigo.page";
 export * from "./caf/convertigo.router";
 export * from "./caf/convertigo.routingtable";
+export * from "./caf/convertigo.base";
+export * from "./caf/convertigo.utils";
